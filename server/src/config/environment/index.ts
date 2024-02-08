66b52508127685e8c10f6d8dd5add4a0883bdd46
nullsploit01 <harshaldharmik099@gmail.com>
2024-02-09 01:54:40 +0530

    add environment and logging config

---
+++ server/src/config/environment/index.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  PORT: process.env.PORT || 5001,
+  NODE_ENV: process.env.NODE_ENV || 'local',
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+import { environment } from 'src/config/environment';
+import { logger } from 'src/config/logger';
 
 const app = express();
-
-const PORT = process.env.PORT || 5001;
 
 app.get('/ping', (req, res) => {
   return res.json('pong');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(environment.PORT, () => {
+  logger.info(`Server listening on port ${environment.PORT}`);
 });
