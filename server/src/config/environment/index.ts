2c000cdfecb1e36d36ef2ebac73832dae6c4fd50
nullsploit01 <harshaldharmik099@gmail.com>
2024-02-12 01:47:15 +0530

    fix env

--- server/src/config/environment/index.ts
+++ server/src/config/environment/index.ts
@@ -1,18 +1,18 @@
 import dotenv from 'dotenv'
 
-const nodeEnv = process.env.NODE_ENV || 'local'
+const nodeEnv = process.env.NODE_ENV || 'development'
 const envFilePath = `.env.${nodeEnv}`
 
 dotenv.config({ path: envFilePath })
 
-if (!process.env.GOOGLE_PROJECT_ID || !process.env.GOOGLE_PROJECT_LOCATION) {
+if (!process.env.PROJECT_ID || !process.env.PROJECT_LOCATION) {
   throw new Error('Google project config is not defined')
 }
 
 export const environment = {
-  PORT: process.env.PORT || 5001,
-  NODE_ENV: process.env.NODE_ENV || 'local',
-  GOOGLE_PROJECT_ID: process.env.GOOGLE_PROJECT_ID,
-  GOOGLE_PROJECT_LOCATION: process.env.GOOGLE_PROJECT_LOCATION,
+  PORT: process.env.PORT || 8080,
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  GOOGLE_PROJECT_ID: process.env.PROJECT_ID,
+  GOOGLE_PROJECT_LOCATION: process.env.PROJECT_LOCATION,
   ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN
 }
