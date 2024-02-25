3e1855ac18c37576d217a05b4a8d900396f58215
nullsploit01 <harshaldharmik099@gmail.com>
2024-02-25 23:04:13 +0530

    add validation for itinerary

--- server/src/services/itinerary/index.ts
+++ server/src/services/itinerary/index.ts
@@ -1,4 +1,6 @@
 import { vertexAIClient } from 'src/client/vertex-ai'
+import { BadRequestError } from 'src/errors/bad-request'
+import { NotFoundError } from 'src/errors/not-found'
 
 class ItineraryService {
   getItinerary = async (location: string, from: Date, to: Date) => {
@@ -9,7 +11,7 @@ class ItineraryService {
           parts: [
             {
               text: `Plan a trip to ${location} from ${from} to ${to}. 
-                    Generate response in json of format. If location is not valid/found return status 400 else 200.
+                    Generate response in json of format. If location is not valid return status 400, if not found return status 404, else 200.
                     {status: value, itinerary: [{day: value (Name of Day, Month DD YYYY), activities: [{name: value, location: value, description: value, time: value}]}`
             }
           ]
@@ -28,7 +30,24 @@ class ItineraryService {
       }
     })
 
-    return JSON.parse(itinerary.replace(/```/g, ''))
+    return this.validateItinerary(itinerary)
+  }
+
+  private validateItinerary = (itinerary: any) => {
+    itinerary = JSON.parse(itinerary.replace(/```/g, ''))
+
+    switch (itinerary.status) {
+      case 400:
+        throw new BadRequestError('Location is invalid')
+
+      case 404:
+        throw new NotFoundError('Location not found')
+
+      default:
+        break
+    }
+
+    return itinerary
   }
 }
 
